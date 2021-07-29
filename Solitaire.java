8af7e32b228e0ce7df788011700fb9b3496f76cb
Gam Aung San <59454345+gamaungsan@users.noreply.github.com>
2021-07-29 11:06:00 -0400

    Update Solitaire.java

--- Solitaire.java
+++ Solitaire.java
@@ -1,62 +1,73 @@
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.awt.Graphics;
 
 public class Solitaire {
-    static public DeckPile deckPile;
-    static public DiscardPile discardPile;
-    static public TableauPile tableau[];
-    static public SuitPile suitPile[];
-    static public CardPile allPiles[];
-    private JFrame window;
+    public static Deck deckPile;
+    public static DiscardPile discardPile;
+    public static ArrayList<TableauPile> tableau;
+    public static ArrayList<SuitPile> suitPile;
+    public static ArrayList<CardPile> allPiles;
+    private JFrame game;
 
     public static void main(String args[]) {
-        Solitaire world = new Solitaire();
+        new Solitaire();
     }
 
     public Solitaire() {
-        window = new SolitaireFrame();
+        game = new SolitaireFrame();
         init();
-        window.setVisible(true);
+        game.setVisible(true);
     }
 
     public void init() {
-        allPiles = new CardPile[13];
-        suitPile = new SuitPile[4];
-        tableau = new TableauPile[7];
+        allPiles = new ArrayList<CardPile>();
+        suitPile = new ArrayList<SuitPile>();
+        tableau = new ArrayList<TableauPile>();
 
-        allPiles[0] = deckPile = new DeckPile(15, 25);
-        allPiles[1] = discardPile = new DiscardPile(100, 45);
-        for (int i = 0; i < 4; i++)
-            allPiles[2 + i] = suitPile[i] = new SuitPile(250 + (Card.width + 10) * i, 40);
+        deckPile = new Deck(15, 45);
+        allPiles.add(deckPile);
+        discardPile = new DiscardPile(100, 45);
+        allPiles.add(discardPile);
+        for (int i = 0; i < 4; i++) {
+            suitPile.add(new SuitPile(250 + (Card.width + 10) * i, 40));
+            allPiles.add(suitPile.get(i));
+        }
 
-        for (int i = 0; i < 7; i++)
-            allPiles[6 + i] = tableau[i] = new TableauPile(15 + (Card.width + 5) * i, Card.height + 40, i + 1);
+        for (int i = 0; i < 7; i++) {
+            tableau.add(new TableauPile(15 + (Card.width + 5) * i, Card.height + 40, i + 1));
+            allPiles.add(tableau.get(i));
+        }
 
     }
 
     class SolitaireFrame extends JFrame {
 
-        private class MouseKeeper extends MouseAdapter {
+        private class Mouse extends MouseAdapter {
             public void mousePressed(MouseEvent e) {
                 int x = e.getX();
                 int y = e.getY();
                 for (int i = 0; i < 13; i++)
-                    if (allPiles[i].includes(x, y)) {
-                        allPiles[i].select(x, y);
+                    if (allPiles.get(i).includes(x, y)) {
+                        allPiles.get(i).select();
                         repaint();
                     }
+
+                if (checkWin())
+                    JOptionPane.showMessageDialog(null, "You won!");
             }
         }
 
         public SolitaireFrame() {
             setSize(630, 600);
             setTitle("Solitaire");
-            addMouseListener(new MouseKeeper());
+            addMouseListener(new Mouse());
             JButton restartButton = new JButton("New Game");
             restartButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
@@ -83,8 +94,22 @@ public class Solitaire {
         public void paint(Graphics g) {
             super.paint(g);
             for (int i = 0; i < 13; i++) {
-                allPiles[i].display(g);
+                allPiles.get(i).display(g);
             }
+        }
+
+        public boolean checkWin() {
+            boolean win = false;
+            for (int i = 0; i < 4; i++) {
+                int size = suitPile.get(i).getSize();
+                if (size == 13)
+                    win = true;
+                else {
+                    win = false;
+                    break;
+                }
+            }
+            return win;
         }
 
     }
